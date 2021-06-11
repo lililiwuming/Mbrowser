817d89ed0c58b2b53af07c7607d1f3e644c61366
lililiwuming <62045515+lililiwuming@users.noreply.github.com>
2021-06-11 19:57:48 +0800

    init

---
+++ egwang186.sh
@@ -0,0 +1,12 @@
+cd $HOME/work/
+wget -O ./egwang186/zycj/免嗅探.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/免嗅探.txt
+wget -O ./egwang186/zycj/新重组数据.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/新重组数据.txt
+wget -O ./egwang186/zycj/资源网采集.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/资源网采集.txt
+wget -O ./egwang186/wwsh/万物生-首页.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-首页.txt
+wget -O ./egwang186/wwsh/万物生-首页推荐.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-首页推荐.txt
+wget -O ./egwang186/wwsh/万物生-选集列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-选集列表.txt
+wget -O ./egwang186/wwsh/万物生-选集列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-选集列表.txt
+wget -O ./egwang186/wwsh/万物生-搜索列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-搜索列表.txt
+wget -O ./egwang186/wwsh/万物生-头部导航.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-头部导航.txt
+wget -O ./egwang186/wwsh/万物生-单项列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-单项列表.txt
+wget -O ./egwang186/wwsh/万物生-免嗅探.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-免嗅探.txt
