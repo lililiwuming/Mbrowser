c5c76a49611aa840262a46af464e514675ac2c84
lililiwuming <62045515+lililiwuming@users.noreply.github.com>
2021-08-22 02:15:33 +0800

    up egwang186

--- egwang186.sh
+++ egwang186.sh
@@ -1,12 +1,14 @@
-cd $HOME/work/
-wget -O ./egwang186/zycj/免嗅探.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/免嗅探.txt
-wget -O ./egwang186/zycj/新重组数据.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/新重组数据.txt
-wget -O ./egwang186/zycj/资源网采集.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/资源网采集.txt
-wget -O ./egwang186/wwsh/万物生-首页.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-首页.txt
-wget -O ./egwang186/wwsh/万物生-首页推荐.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-首页推荐.txt
-wget -O ./egwang186/wwsh/万物生-选集列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-选集列表.txt
-wget -O ./egwang186/wwsh/万物生-选集列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-选集列表.txt
-wget -O ./egwang186/wwsh/万物生-搜索列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-搜索列表.txt
-wget -O ./egwang186/wwsh/万物生-头部导航.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-头部导航.txt
-wget -O ./egwang186/wwsh/万物生-单项列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-单项列表.txt
-wget -O ./egwang186/wwsh/万物生-免嗅探.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-免嗅探.txt
+cd $HOME/work/egwang186
+wget -O ./zycj/免嗅探.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/免嗅探.txt
+wget -O ./zycj/新重组数据.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/新重组数据.txt
+wget -O ./zycj/资源网采集.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/资源网采集.txt
+wget -O ./wwsh/万物生-首页.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-首页.txt
+wget -O ./wwsh/万物生-首页推荐.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-首页推荐.txt
+wget -O ./wwsh/万物生-选集列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-选集列表.txt
+wget -O ./wwsh/万物生-选集列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-选集列表.txt
+wget -O ./wwsh/万物生-搜索列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-搜索列表.txt
+wget -O ./wwsh/万物生-头部导航.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-头部导航.txt
+wget -O ./wwsh/万物生-单项列表.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-单项列表.txt
+wget -O ./wwsh/万物生-免嗅探.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-免嗅探.txt
+git add .
+git commit -m "up egwang186"
