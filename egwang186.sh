c7a94bcfd29f7cdd1d5b239c2b9dd3053d3e8bb6
lililiwuming <62045515+lililiwuming@users.noreply.github.com>
2021-12-28 10:37:28 +0800

    up egwang186

--- egwang186.sh
+++ egwang186.sh
@@ -4,7 +4,7 @@ wget -O ./zycj/总代码.txt https://egwang186.coding.net/p/egwang186/d/iptv/git
 wget -O ./zycj/资源网采集.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/zywcj/资源网采集.txt
 wget -O ./wwsh/万物生-首页.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/万物生-首页.txt
 wget -O ./wwsh/万物生-总代码.txt https://egwang186.coding.net/p/egwang186/d/iptv/git/raw/master/js2.0/总代码.txt
-wget -O ./index.html https://gitee.com/egwang186/egwang186/raw/master/src/index.html
+wget -O ./endex.html https://gitee.com/egwang186/egwang186/raw/master/src/index.html
 
 # sed 's/uu.indexOf("cokemv.me")!=-1||//' ./wwsh/万物生-总代码.txt >./My/万物生-总代码.txt
 git add .
